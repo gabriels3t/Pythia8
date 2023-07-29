1c9518c3d72a1cf9244035db49e4f2960b7c0eb2
Quark <Quark@mailfence.com>
2023-07-29 20:29:15 -0300

    Dados obtidos e exportados por .dat

--- LEP/main.py
+++ LEP/main.py
@@ -18,13 +18,32 @@ pythia.readString("WeakSingleBoson:ffbar2gmZ = on") # produzir boson Z
 pythia.init() # Inicialização 
 
 
-n_event = 10 # Numero de eventos 
+"""
+pt, px, py, pz, pt, eta(rapidez), phi (ângulo azimutal)
+"""
 
+n_event = 10 # Numero de eventos 
+id =[];pT = [];px = [];py = [];pz = [];eta = [];phi = [];event=[]
 #loop event
 for i in range(n_event):
     if not pythia.next(): continue
     #loop particle
     for particle in pythia.event:
         if  particle.isFinal(): # particula estavel  
-           print(particle.id())
-           print(20*"=====")
+           event.append(i)
+           id.append(particle.id())
+           pT.append(particle.pT())
+           px.append(particle.px())
+           py.append(particle.py())
+           pz.append(particle.pz())
+           eta.append(particle.eta())
+           phi.append(particle.phi())
+
+np.savetxt("data/id.dat",id,delimiter=',')
+np.savetxt("data/pT.dat",pT,delimiter=',')
+np.savetxt("data/px.dat",px,delimiter=',')
+np.savetxt("data/py.dat",py,delimiter=',')
+np.savetxt("data/pz.dat",pz,delimiter=',')
+np.savetxt("data/eta.dat",eta,delimiter=',')
+np.savetxt("data/phi.dat",phi,delimiter=',')
+np.savetxt("data/event.dat",event,delimiter=',')
